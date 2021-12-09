e27faf0dcf2a16e787c1239a8d5dc47edbd7097a
dadwindad <vongpramate@gmail.com>
2021-12-09 11:58:06 +0700

    LAB3

---
+++ lab3/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Car from "./components/Car";
+
+const numbers = [10, 20, 30];
+const text = (
+  <>
+    <div>Hello World !!! React {numbers[0] + numbers[2]}</div>
+    <h3 className="red">My name is Drusawin</h3>
+  </>
+);
+
+ReactDOM.render(<Car brand="Toyota" />, document.getElementById("root"));
